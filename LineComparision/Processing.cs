00b0cb0c58af76b6f29ac18cd676013dd21101cb
Senendra <senendra.007@gmail.com>
2020-10-02 02:26:03 +0530

    [Senendra] Refactor: Updating program with OOPs concept

---
+++ LineComparision/Processing.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LineComparision
+{
+    class Processing
+    {
+        //Variables
+        public int x1, y1, x2, y2, lineNumber;
+        double length, line1, line2;
+        Dictionary<int, double> line = new Dictionary<int, double>();
+        public void setLine(int lineNumber)
+        {
+            Console.WriteLine("Enter values of point x1 and y1  for line "+lineNumber+" :");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter values of point x2 and y2  for line " + lineNumber + " :");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            length = findLenght();
+            line.Add( lineNumber , length);
+        }
+        public int findLenght()
+        {
+             return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+        public void lineCompare()
+        {
+            line1 = line[1];
+            line2 = line[2];
+            double diffInLength = line1.CompareTo(line2);
+            if (diffInLength == 0)
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else if (diffInLength > 0)
+            {
+                Console.WriteLine("Line 1 is greater in length than line 2");
+            }
+            else
+            {
+                Console.WriteLine("Line 2 is greater in length than line 1");
+            }
+        }
+    }
+}
--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -6,37 +6,10 @@ namespace LineComparision
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparision Computation Program");
-            //Variables
-            int x1, y1, x2, y2 ,x3 , y3, x4, y4;
-            //Geeting inputs for both the points of line 1
-            Console.WriteLine("Enter values of x1 and y1 for line 1 : ");
-            x1 = Convert.ToInt32(Console.ReadLine());
-            y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values of x2 and y2 for line 1 : ");
-            x2 = Convert.ToInt32(Console.ReadLine());
-            y2 = Convert.ToInt32(Console.ReadLine());
-            double line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            //Geeting inputs for both the points of line 2
-            Console.WriteLine("Enter values of x1 and y1 for line 2 : ");
-            x3 = Convert.ToInt32(Console.ReadLine());
-            y3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values of x2 and y2 for line 2 : ");
-            x4 = Convert.ToInt32(Console.ReadLine());
-            y4 = Convert.ToInt32(Console.ReadLine());
-            double line2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            int diffInLength = line1.CompareTo(line2);
-            if (diffInLength == 0)
-            {
-                Console.WriteLine("Lines are equal");
-            }
-            else if (diffInLength > 0)
-            {
-                Console.WriteLine("Line 1 is greater in length than line 2");
-            }
-            else
-            {
-                Console.WriteLine("Line 2 is greater in length than line 1");
-            }
+            Processing process = new Processing();
+            process.setLine(1);
+            process.setLine(2);
+            process.lineCompare();
         }
     }
 }
